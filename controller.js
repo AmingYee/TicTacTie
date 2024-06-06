152a5dbfe3019f68d626aaa79fafac93a5287e33
AmingYee <wenm0007@stud.kea.dk>
2024-06-06 22:52:14 +0200

    updated to using the correct checkwinner and draw function

--- controller.js
+++ controller.js
@@ -79,7 +79,7 @@ class TicTacToeController {
     }
 
     generateGameTreeHelper(node, currentPlayer, depth) {
-        if (depth >= this.maxDepth || this.model.checkWinner2(this.playerSymbol) || this.model.checkWinner2(this.aiSymbol) || this.model.checkDraw()) {
+        if (depth >= this.maxDepth || this.model.checkWinner2(this.playerSymbol, node.board) || this.model.checkWinner2(this.aiSymbol, node.board) || this.model.checkDraw2(node.board)) {
             return;
         }
 
